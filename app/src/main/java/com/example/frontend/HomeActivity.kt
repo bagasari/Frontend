3c3d3ceaa4993f9e718cb1222343a3142c56d5e7
yeopseung <kwak991021@gmail.com>
2023-04-07 16:18:59 +0900

    feat: LoginActivity, HomeActivity 생성

--- app/src/main/java/com/example/frontend/HomeActivity.kt
+++ app/src/main/java/com/example/frontend/HomeActivity.kt
@@ -1,7 +1,11 @@
 package com.example.frontend
 
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
 
-class HomeActivity : AppCompatActivity(){
-
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
 }
---
+++ app/src/main/java/com/example/frontend/LoginActivity.kt
@@ -0,0 +1,28 @@
+package com.example.frontend
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+
+class LoginActivity : AppCompatActivity() {
+    private val tag:String = "LoginActivity"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        val btnSignIn = findViewById<Button>(R.id.activity_login_btnSignIn)
+        btnSignIn.setOnClickListener {
+            Log.d(tag, "로그인 버튼 클릭")
+
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
+
+        val btnSignUp = findViewById<Button>(R.id.activity_login_btnSignUp)
+        btnSignUp.setOnClickListener {
+            Log.d(tag, "회원가입 버튼 클릭")
+        }
+    }
+}
